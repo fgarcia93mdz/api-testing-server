848e92f4203c1bdebab0c298078edc031ab5f0f7
Franco <frangarcia93@icloud.com>
2024-03-17 15:14:22 -0300

    Cambio de puerto

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const usersRoutes = require('./src/routes/users');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 
 //test
 
